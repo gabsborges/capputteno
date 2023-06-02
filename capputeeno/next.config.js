d4a8ed3e7b4c76ca940175c873452893dab86fed
gabsborges <gabriel.borges_96@hotmail.com>
2023-06-02 14:29:05 -0300

    feat: creating header

--- capputeeno/next.config.js
+++ capputeeno/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+compiler: {
+    styledComponent: true
+}
 
 module.exports = nextConfig
--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,7 +1,11 @@
+import { Header } from '@/components/header'
 import './globals.css'
 import { Saira } from 'next/font/google'
 
-const saira = Saira({ subsets: ['latin'] })
+const saira = Saira({ 
+  weight: ['300', '400', '500', '600'],
+  subsets: ['latin'] 
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={saira.className}>{children}</body>
+      <body className={saira.className}>
+        <Header/>
+        {children}
+        
+        </body>
     </html>
   )
 }
